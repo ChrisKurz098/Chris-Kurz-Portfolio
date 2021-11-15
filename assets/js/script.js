d311bf91b0a451a9ef32a361a28f27fef5ed432d
CKURZ <ckurz098@gmail.com>
2021-11-14 17:09:30 -0800

    attempted JS code effect

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+// var newSize=120, originalSize=167.13;
+// window.onscroll = function() {
+//     if (window.pageYOffset<=100){
+//         newSize-=1;
+//         document.getElementById("profile-pic").style.height = newSize+'px';
+//         document.getElementById("profile-pic").style.width = newSize+'px';
+//     } else { 
+//         document.getElementById("profile-pic").style.height = originalSize+'px';
+//         document.getElementById("profile-pic").style.width = originalSize+'px';
+//     }
+//     console.log(window.pageYOffset);
+// }
+
